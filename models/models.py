0ef8f13ee97116fbe85d515a320d71c89210bf80
Antonio Miras <amiras@ferba.es>
2017-08-10 14:13:36 +0200

    Shipment: Added forwarder and transport fields

--- models/models.py
+++ models/models.py
@@ -5,9 +5,17 @@ from odoo import models, fields
 class Shipment(models.Model):
     _name = 'export_and_shipping.shipment'
 
-    awb = fields.Char(string="Airway Bill")
+    awb = fields.Char(string="Airway Bill / Book")
     date = fields.Date(string="Departure", default=fields.Date.today)
     by_plane = fields.Boolean(string="By Plane", default=True)
 
     perishable = fields.Many2one('product.product',
         ondelete='set null', string="Perishable", index=True)
+
+    forwarder = fields.Many2one('res.partner',
+        ondelete='set null', string="Forwarder", index=True,
+        domain=['&', ('supplier', '=', True), ('category_id.name', 'ilike', "Forwarder")])
+
+    transport = fields.Many2one('res.partner',
+        ondelete='set null', string="Transport", index=True, required=True,
+        domain = ['&', ('supplier', '=', True), ('category_id.name', 'ilike', "Transport")])
