f28d16fe567c32fbb05b62c702969c7ed8215ff8
Antonio Miras <amiras@@ferba.es>
2017-08-10 14:51:10 +0200

    Shipments: More fields

--- models/models.py
+++ models/models.py
@@ -6,11 +6,21 @@ class Shipment(models.Model):
     _name = 'export_and_shipping.shipment'
 
     awb = fields.Char(string="Airway Bill / Book")
-    date = fields.Date(string="Departure", default=fields.Date.today)
+    flight_no = fields.Char(string="Flight / Container Number")
     by_plane = fields.Boolean(string="By Plane", default=True)
+
+    approx_weight = fields.Integer(string="Approx. Weight")
+
+    retrieval_date = fields.Date(string="Retrieval", default=fields.Date.today)
+    departure_date = fields.Date(string="Departure", default=fields.Date.today)
+    arrival_date = fields.Date(string="Arrival", default=fields.Date.today)
 
     perishable = fields.Many2one('product.product',
         ondelete='set null', string="Perishable", index=True)
+
+    customer = fields.Many2one('res.partner',
+        ondelete='set null', string="Customer", index=True,
+        domain=[('customer', '=', True)])
 
     forwarder = fields.Many2one('res.partner',
         ondelete='set null', string="Forwarder", index=True,
