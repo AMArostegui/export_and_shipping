742338002b76570fdd255165494c379bb42bef36
Antonio Miras <amiras@@ferba.es>
2017-09-04 16:40:54 +0200

    Default data: Products varieties

--- models/loader.py
+++ models/loader.py
@@ -7,11 +7,12 @@ class Loader:
     TAG_VENDOR = {}
     CAT_PRODUCT = []
 
-    def __init__(self, environment):
-        self.environment = environment
+    def __init__(self, shipment):
+        self.environment = shipment.env
+        self.module_name = shipment._module
 
     def __enter__(self):
-        pathname = os.path.join(u'export_and_shipping', u'data/default_post_install.xml')
+        pathname = os.path.join(self.module_name, u'data/default_post_install.xml')
         self.fp = file_open(pathname)
         self.tree = ET.fromstring(self.fp.read())
         return self
@@ -20,8 +21,11 @@ class Loader:
         self.fp.close()
 
     @staticmethod
-    def read_categories():
-        pathname = os.path.join(u'export_and_shipping', u'data/default.xml')
+    def read_categories(model_name):
+        idx = model_name.find(".")
+        module_name = model_name[:idx]
+
+        pathname = os.path.join(module_name, u'data/default.xml')
         fp = file_open(pathname)
 
         try:
@@ -44,35 +48,63 @@ class Loader:
         res_partners_categories = self.environment["res.partner.category"]
 
         for cat_id, cat_name in Loader.TAG_VENDOR.iteritems():
-            category = res_partners_categories.search([('name', 'ilike', cat_name)])
+            res_partner_category = res_partners_categories.search([('name', 'ilike', cat_name)])
             find_nodes_xpath = ".//data/vendors/category[@id='{0}']".format(cat_id)
             cat_nodes = self.tree.findall(find_nodes_xpath)
             cat_nodes = cat_nodes[0]
             for cat_node in cat_nodes._children:
-                new_partner = { u'type': u'contact', u'is_company': False, u'customer': False, u'supplier': True, u'employee': False,
-                               u'name': cat_node.text, u'category_id': [(4, [category.id])] }
-                domain_check_exists = [('name', 'ilike', new_partner['name'])]
+                res_partner_obj = { u'type': u'contact', u'is_company': False, u'customer': False, u'supplier': True, u'employee': False,
+                               u'name': cat_node.text, u'category_id': [(4, [res_partner_category.id])] }
+                domain_check_exists = [('name', 'ilike', res_partner_obj['name'])]
                 if res_partners.search_count(domain_check_exists) == 0:
-                    res_partners.create(new_partner)
+                    res_partners.create(res_partner_obj)
 
     def add_default_products(self):
-        product_template = self.environment["product.template"]
-        product_category = self.environment["product.category"]
-        cat_id = product_category.search([('name', 'ilike', Loader.CAT_PRODUCT[0])])
+        product_templates = self.environment["product.template"]
+        product_categories = self.environment["product.category"]
+        cat_id = product_categories.search([('name', 'ilike', Loader.CAT_PRODUCT[0])])
 
         find_nodes_xpath = ".//data/products/product"
         product_nodes = self.tree.findall(find_nodes_xpath)
 
         for product_node in product_nodes:
-            new_product_template = { u'sale_ok': True, u'purchase_ok': True,
+            product_template_obj = { u'sale_ok': True, u'purchase_ok': True,
                                      u'name': product_node.attrib["name"], u'categ_id': cat_id.id }
-            product_exists = [('name', 'ilike', new_product_template['name'])]
-            if product_template.search_count(product_exists) != 0:
+            product_exists = [('name', 'ilike', product_template_obj['name'])]
+            if product_templates.search_count(product_exists) != 0:
                 continue
-            product_template.create(new_product_template)
 
+            product_template = product_templates.create(product_template_obj)
+
+            att_val_xmlids = []
             for variety_node in product_node._children:
-                variety = variety_node.attrib["id"]
+                att_val_xmlids.append(variety_node.attrib["id"])
 
+            self.add_varieties(product_template, att_val_xmlids)
 
+    def add_varieties(self, product_template, att_val_xmlids):
+        if len(att_val_xmlids) <= 0:
+            return
 
+        product_products = self.environment["product.product"]
+        old_product_product = product_products.search([('product_tmpl_id', '=', product_template.id)])
+
+        for att_val_xmlid in att_val_xmlids:
+            product_attribute_value = self.environment.ref(self.module_name + '.' + att_val_xmlid)
+            product_attribute = product_attribute_value.attribute_id
+
+            product_attributes_lines = self.environment["product.attribute.line"]
+
+            line_search = product_attributes_lines.search([(u'attribute_id', '=', product_attribute.id), (u'product_tmpl_id', '=', product_template.id)])
+            if len(line_search) == 0:
+                product_attribute_line = product_attributes_lines.create({ u'attribute_id': product_attribute.id, u'product_tmpl_id':product_template.id })
+            else:
+                product_attribute_line = line_search[0]
+
+            new_product_product = product_products.create({ u'sale_ok': True, u'purchase_ok': True, u'product_tmpl_id': product_template.id,
+                                         u'name': product_template.name, u'categ_id': product_template.categ_id.id })
+
+            product_attribute_value.write({u'product_ids': [(4, [new_product_product.id])] })
+            product_attribute_line.write({u'value_ids': [(4, [product_attribute_value.id])]})
+
+        old_product_product.unlink()
--- models/models.py
+++ models/models.py
@@ -25,7 +25,7 @@ class Shipment(models.Model):
         ondelete='set null', string="Customer", index=True,
         domain=[('customer', '=', True)])
 
-    loader.Loader.read_categories()
+    loader.Loader.read_categories(_name)
 
     forwarder = fields.Many2one('res.partner',
         ondelete='set null', string="Forwarder", index=True,
@@ -39,46 +39,13 @@ class Shipment(models.Model):
 
     @api.model
     def view_init(self, fields_list):
-        model_product_template = self.env["product.template"]
-        product_template = model_product_template.search([('name', 'ilike', "Mango")])
-
-        model_product_product = self.env["product.product"]
-        old_product_product = model_product_product.search([('name', 'ilike', "Mango")])
-
-        model_product_attribute = self.env["product.attribute"]
-        product_attribute = model_product_attribute.search([('name', 'ilike', "Tipo Mango")])
-
-        model_product_attributes_line = self.env["product.attribute.line"]
-        product_attribute_line = model_product_attributes_line.create({ u'attribute_id': product_attribute.id, u'product_tmpl_id':product_template.id })
-
-        model_product_attribute_value = self.env["product.attribute.value"]
-
-        new_product_product = model_product_product.create({ u'sale_ok': True, u'purchase_ok': True, u'product_tmpl_id': product_template.id,
-                                     u'name': product_template.name, u'categ_id': product_template.categ_id.id })
-        product_attribute_value = model_product_attribute_value.search([('name', 'ilike', "Keitt")])
-        product_attribute_value.write({u'product_ids': [(4, [new_product_product.id])] })
-        product_attribute_line.write({u'value_ids': [(4, [product_attribute_value.id])]})
-
-        new_product_product = model_product_product.create({ u'sale_ok': True, u'purchase_ok': True, u'product_tmpl_id': product_template.id,
-                                     u'name': product_template.name, u'categ_id': product_template.categ_id.id })
-        product_attribute_value = model_product_attribute_value.search([('name', 'ilike', "Kentt")])
-        product_attribute_value.write({u'product_ids': [(4, [new_product_product.id])] })
-        product_attribute_line.write({u'value_ids': [(4, [product_attribute_value.id])]})
-
-
-        old_product_product.unlink()
-
-        pass
-
-
-
         # if not Shipment._startup:
         #     return
-        #
-        # with loader.Loader(self.env) as load:
-        #     load.add_default_vendors()
-        #     load.add_default_products()
-        #
+
+        with loader.Loader(self) as load:
+            load.add_default_vendors()
+            load.add_default_products()
+
         # Shipment._startup = False
 
     @api.model
